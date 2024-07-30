3dc46a5dd61fc2560892a92c513588e546dc7a28
amalvelloth <amalv414@gmail.com>
2024-07-30 17:15:06 +0530

    custom font

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('scroll', function () {
     const scrollY = window.scrollY;
     document.querySelector('.moon').style.transform = `translate(-50%, -50%) translateY(${scrollY * -0.1}px)`;
-    document    .querySelector('.zenitsu img').style.transform = `translateY(${scrollY * 0.3}px)`;
+    document.querySelector('.zenitsu img').style.transform = `translateY(${scrollY * 0.4}px)`;
 });
